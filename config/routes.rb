a41aa3be85b52a8ab11a03184af1288dd288d0e9
Jamie Pace <jamiejpace@gmail.com>
2021-11-14 14:07:54 -0700

    Add route for users create action

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       get 'forecast', to: 'forecast#index'
       get 'backgrounds', to: 'backgrounds#index'
+      
+      resources :users, only: :create
     end
   end
 end
