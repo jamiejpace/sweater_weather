d11a6f426148ea49c4f1cfca6874283020df277d
Jamie Pace <jamiejpace@gmail.com>
2021-11-15 09:26:01 -0700

    Add to test, add location and forecast to endpoint

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::ActivitiesController < ApplicationController
   def index
-
+    location = MapQuestFacade.get_location(params[:destination])
+    current_forecast = OpenWeatherFacade.get_weather(location.latitude, location.longitude).current
+    activities = BoredFacade.get_two_activitites(current_forecast[:temp])
   end
 end
--- spec/requests/api/v1/activities_spec.rb
+++ spec/requests/api/v1/activities_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'activities endpoint' do
-  it 'retrieves activites for a given city' do
+  it 'retrieves activities for a given city' do
     get '/api/v1/activities', params: {destination: "chicago,il"}
 
     expect(response).to be_successful
+
+    activities = JSON.parse(response.body, symbolize_names: true)
+
+    expect(activities).to be_a(Hash)
   end
 end
