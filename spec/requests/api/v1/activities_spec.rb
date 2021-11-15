a9960c11f857b0a3365853351651c89ae66e2e45
Jamie Pace <jamiejpace@gmail.com>
2021-11-15 09:18:02 -0700

    Change endpoint in test

--- spec/requests/api/v1/activities_spec.rb
+++ spec/requests/api/v1/activities_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe 'activities endpoint', :vcr do
+RSpec.describe 'activities endpoint' do
   it 'retrieves activites for a given city' do
-    get 'api/v1/activities?destination=chicago,il'
+    get '/api/v1/activities', params: {destination: "chicago,il"}
 
     expect(response).to be_successful
   end
