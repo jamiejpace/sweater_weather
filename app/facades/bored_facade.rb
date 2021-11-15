3f29223c73ec17b0473517fcc08fb66e4bc03efe
Jamie Pace <jamiejpace@gmail.com>
2021-11-15 10:09:40 -0700

    Add sad path test, refactor facade

--- app/facades/bored_facade.rb
+++ app/facades/bored_facade.rb
@@ -1,14 +1,14 @@
 class BoredFacade
   class << self
     def get_two_activities(temp)
-      data1 = BoredService.activity_data("relaxation")
-      data2 = if temp >= 60
+      data1 = if temp >= 60
                 BoredService.activity_data("recreational")
               elsif temp >= 50 && temp < 60
                 BoredService.activity_data("busywork")
               else
                 BoredService.activity_data("cooking")
               end
+      data2 = BoredService.activity_data("relaxation")
       [Activity.new(data1), Activity.new(data2)]
     end
   end
--- spec/requests/api/v1/activities_spec.rb
+++ spec/requests/api/v1/activities_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'activities endpoint' do
-  it 'retrieves activities for a given city' do
+  it 'retrieves activities for a given destination' do
     get '/api/v1/activities', params: {destination: "chicago,il"}
 
     expect(response).to be_successful
@@ -9,5 +9,16 @@ RSpec.describe 'activities endpoint' do
     activities = JSON.parse(response.body, symbolize_names: true)
 
     expect(activities).to be_a(Hash)
+    expect(activities).to have_key(:data)
+    expect(activities[:data]).to have_key(:id)
+    expect(activities[:data]).to have_key(:type)
+    expect(activities[:data]).to have_key(:attributes)
+  end
+
+  it 'returns an error if no destination is given' do
+    get '/api/v1/activities'
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
   end
 end
