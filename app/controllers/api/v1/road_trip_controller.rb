5c6ea610810be6820d7f3919b8b69b88a5b9fe54
Jamie Pace <jamiejpace@gmail.com>
2021-11-15 12:46:34 -0700

    Add activity serailizer

---
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -0,0 +1,31 @@
+class Api::V1::RoadTripController < ApplicationController
+  before_action :require_valid_key
+
+  def create
+    trip_time = MapQuestFacade.get_route_data(params[:origin], params[:destination])
+    coordinates = MapQuestFacade.get_location(params[:destination])
+    arrival_weather = OpenWeatherFacade.get_future_weather(coordinates, number_of_hours(trip_time))
+    render json: RoadTripSerializer.new(origin_and_destination, arrival_weather), status: 200
+  end
+
+  private
+
+  def valid_key
+    User.find_by(access_token: params[:api_key])
+  end
+
+  def require_valid_key
+    render json: { error: "Unauthorized request" }, status: 401 unless valid_key
+  end
+
+  def number_of_hours(time)
+    time.split(':').first.to_i
+  end
+
+  def origin_and_destination
+    {
+      origin: params[:origin],
+      destination: params[:destination]
+    }
+  end
+end
---
+++ app/serializers/road_trip_serializer.rb
@@ -0,0 +1,13 @@
+class ActivitySerializer
+  def self.new(destination, current_forecast, activities)
+    {
+      "data": {
+        "id": nil,
+        "type": "roadtrip",
+        "attributes": {
+          require 'pry'; binding.pry
+        }
+      }
+    }
+  end
+end
