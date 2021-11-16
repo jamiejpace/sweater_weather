066850b25b829d512bc1d17f947f362d78301d4e
Jamie Pace <jamiejpace@gmail.com>
2021-11-16 09:46:58 -0700

    Add tests for all temp ranges in bored facade

--- spec/facades/bored_facade_spec.rb
+++ spec/facades/bored_facade_spec.rb
@@ -1,12 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe 'bored facade', :vcr do
-  it 'returns a two activity objects in an array' do
+  it 'returns a two activity objects in an array for temp < 50' do
     response = BoredFacade.get_two_activities(33)
+    expect(response).to be_an(Array)
+    expect(response.count).to eq(2)
+    expect(response.first).to be_an(Activity)
+    expect(response.first.type).to eq("cooking")
+    expect(response.last).to be_an(Activity)
+  end
+
+  it 'returns a two activity objects in an array for temp > 50 and temp < 60' do
+    response = BoredFacade.get_two_activities(55)
 
     expect(response).to be_an(Array)
     expect(response.count).to eq(2)
     expect(response.first).to be_an(Activity)
+    expect(response.first.type).to eq("busywork")
+    expect(response.last).to be_an(Activity)
+  end
+
+  it 'returns a two activity objects in an array for temp > 60' do
+    response = BoredFacade.get_two_activities(70)
+
+    expect(response).to be_an(Array)
+    expect(response.count).to eq(2)
+    expect(response.first).to be_an(Activity)
+    expect(response.first.type).to eq("recreational")
     expect(response.last).to be_an(Activity)
   end
 end
