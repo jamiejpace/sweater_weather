24b3a6e8b44615853c2f585472686e81be449c56
Jamie Pace <jamiejpace@gmail.com>
2021-11-15 20:54:05 -0700

    Refactor serializers to reflect poro changes and sad path

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -5,9 +5,37 @@ class ForecastSerializer
         "id": nil,
         "type": "forecast",
         "attributes": {
-          "current_weather": forecast.current_weather,
-          "daily_weather": forecast.daily_weather,
-          "hourly_weather": forecast.hourly_weather
+          "current_weather": {
+            "datetime": forecast.current.datetime,
+            "sunrise": forecast.current.sunrise,
+            "sunset": forecast.current.sunset,
+            "temperature": forecast.current.temperature,
+            "feels_like": forecast.current.feels_like,
+            "humidity": forecast.current.humidity,
+            "uvi": forecast.current.uvi,
+            "visibility": forecast.current.visibility,
+            "conditions": forecast.current.conditions,
+            "icon": forecast.current.icon
+          },
+          "daily_weather": forecast.daily.map do |d|
+            {
+              "date": d.date,
+              "sunrise": d.sunrise,
+              "sunset": d.sunset,
+              "max_temp": d.max_temp,
+              "min_temp": d.min_temp,
+              "conditions": d.conditions,
+              "icon": d.icon
+            }
+          end,
+          "hourly_weather": forecast.hourly.map do |h|
+            {
+              "time": h.time,
+              "temperature": h.temperature,
+              "conditions": h.conditions,
+              "icon": h.icon
+            }
+          end
         }
       }
     }
--- app/serializers/road_trip_serializer.rb
+++ app/serializers/road_trip_serializer.rb
@@ -1,17 +1,31 @@
 class RoadTripSerializer
-  def self.new(locations, time, arrival_weather)
+  def self.new(origin, destination, time, arrival_weather)
     {
       "data": {
         "id": nil,
         "type": "roadtrip",
         "attributes": {
-          "start_city": locations[:origin],
-          "end_city": locations[:destination],
+          "start_city": origin,
+          "end_city": destination,
           "travel_time": time,
           "weather_at_eta": {
-            "temperature": arrival_weather[:temp],
-            "conditions": arrival_weather[:weather].first[:description]
+            "temperature": arrival_weather.temperature,
+            "conditions": arrival_weather.conditions
           }
+        }
+      }
+    }
+  end
+
+  def self.no_route(origin, destination)
+    {
+      "data": {
+        "id": nil,
+        "type": "roadtrip",
+        "attributes": {
+          "start_city": origin,
+          "end_city": destination,
+          "travel_time": "impossible route"
         }
       }
     }
