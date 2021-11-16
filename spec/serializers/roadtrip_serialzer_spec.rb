1d7603834325ccde385228a9901afe6eb41ea460
Jamie Pace <jamiejpace@gmail.com>
2021-11-16 11:44:31 -0700

    Add sad path test for raodtrip serializer

--- spec/serializers/roadtrip_serialzer_spec.rb
+++ spec/serializers/roadtrip_serialzer_spec.rb
@@ -24,4 +24,23 @@ RSpec.describe 'roadtrip serializer', :vcr do
     expect(roadtrip_json[:data][:attributes][:weather_at_eta]).to have_key(:conditions)
   end
 
+  it 'serializes roadtrip with impossible route' do
+    roadtrip_json = JSON.parse(RoadTripSerializer.no_route("denver,co", "longdon, england").to_json, symbolize_names: true)
+
+    expect(roadtrip_json).to be_a(Hash)
+    expect(roadtrip_json).to have_key(:data)
+    expect(roadtrip_json[:data]).to be_a(Hash)
+    expect(roadtrip_json[:data]).to have_key(:id)
+    expect(roadtrip_json[:data][:id]).to eq(nil)
+    expect(roadtrip_json[:data]).to have_key(:type)
+    expect(roadtrip_json[:data][:type]).to eq("roadtrip")
+    expect(roadtrip_json[:data]).to have_key(:attributes)
+    expect(roadtrip_json[:data][:attributes]).to be_a(Hash)
+    expect(roadtrip_json[:data][:attributes]).to have_key(:start_city)
+    expect(roadtrip_json[:data][:attributes]).to have_key(:end_city)
+    expect(roadtrip_json[:data][:attributes]).to have_key(:travel_time)
+    expect(roadtrip_json[:data][:attributes][:travel_time]).to eq("impossible route")
+    expect(roadtrip_json[:data][:attributes]).to_not have_key(:weather_at_eta)
+  end
+
 end
